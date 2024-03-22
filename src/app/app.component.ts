1799068dd2fb464a0ea32fe5cb46e8b4c81949e4
Jota Santos <jailsonsantosdpge@outlook.com>
2024-03-22 13:09:58 -0400

    mais ajustes de compoentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+// components
+import { NavSidebarComponent } from './components/layout/nav-sidebar/nav-sidebar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { NavSuperiorComponent } from './components/layout/nav-superior/nav-superior.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavSidebarComponent, NavSuperiorComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'appstack-angular';
