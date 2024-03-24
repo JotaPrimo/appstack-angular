b43a27f75f78f5fac0f4d05c64e79f88687acefe
jota santos <gestald118@email.com>
2024-03-24 10:52:51 -0400

    adicionando with e heith padrão icons

---
+++ src/app/components/layout/shared/icons/list-icon/list-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-icon.component.html',
+})
+export class ListIconComponent {
+  width: number = 24;
+  height: number = 24;
+}
