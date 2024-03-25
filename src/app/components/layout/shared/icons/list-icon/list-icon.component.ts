9883c5baf19e77d664ada72df8c3fbdce2e4a381
jota santos <gestald118@email.com>
2024-03-24 20:50:10 -0400

    adicionando icones e properties

--- src/app/components/layout/shared/icons/list-icon/list-icon.component.ts
+++ src/app/components/layout/shared/icons/list-icon/list-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list-icon',
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './list-icon.component.html',
 })
 export class ListIconComponent {
-  width: number = 24;
-  height: number = 24;
+  @Input() width: number = 24;  
+  @Input() height: number = 24;
 }
